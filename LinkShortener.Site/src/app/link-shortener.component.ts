15a427c44f83ace689d3b28843943be49d66cdc6
Alexander Mineev <alexandrmineev@gmail.com>
2017-08-04 14:38:02 +0300

    add main page layout

---
+++ LinkShortener.Site/src/app/link-shortener.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'link-shortener',
+  templateUrl: './link-shortener.component.html'
+})
+export class LinkShortenerComponent {
+
+}
